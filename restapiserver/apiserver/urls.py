59fd18fffc4844d79d48a15b2e0f49ab69216013
paullabkorea <paullabkorea@gmail.com>
2020-03-04 12:43:34 +0000

    update

--- restapiserver/apiserver/urls.py
+++ restapiserver/apiserver/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from main.views import data_list
+from medicine.views import pharmacy_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', data_list),
+    path('p/', pharmacy_list),
 ]
